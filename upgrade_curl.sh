95d22acc870eb7a37ff4f5127ced30fae2c89753
dimahvatit <katorgin.dmitry@gmail.com>
2023-11-09 17:36:08 +0300

    download src instad of "git clone" + refactor

--- upgrade_curl.sh
+++ upgrade_curl.sh
@@ -1,42 +1,77 @@
 #!/bin/bash
 
-# Проверяем, было ли передано два аргумента
-if [ "$#" -ne 2 ]; then
-  echo "Ошибка: Необходимо передать два аргумента - текущую и новую версию curl"
+download_src() {
+  echo "Загрузка исходников"
+  rm -rf ./src/*
+  cd ./src
+  wget $1
+  local TAR_NAME=$(ls)
+  tar xzf $TAR_NAME && rm $TAR_NAME
+  local SRC_DIR_NAME=$(ls)
+  mv $SRC_DIR_NAME/* ./
+  mv $SRC_DIR_NAME/.* ./
+  rm -rf $SRC_DIR_NAME
+  cd ../
+}
+
+remove_image () {
+  # Удаляем старый образ Docker, если он существует
+  if [[ "$(docker images -q $1 2> /dev/null)" == "" ]]; then
+    echo "Старый образ $1 не найден"
+  else
+    echo "Удаляем старый образ $1..."
+    docker rmi $1
+  fi
+}
+
+update_path () {
+  # Проверяем, содержит ли $PATH директорию $CURL_DIR
+  if grep -q "export PATH=\$HOME/bin/curl:\$PATH" ~/.bashrc; then
+    echo "Директория $1 уже содержится в PATH"
+  else
+    echo 'export PATH=$HOME/bin/curl:$PATH' >> ~/.bashrc
+    echo "Директория $1 добавлена к PATH"
+    echo "Требуется релогин"
+  fi
+}
+
+main () {
+  local CURL_DIR="$HOME/bin/curl"
+  local CURRENT_VERSION="$1"
+  local LATEST_VERSION="$2"
+  local TAR_URL="$3"
+  local EXISTING_IMAGE="curl:$CURRENT_VERSION"
+
+  echo CURRENT_VERSION=$CURRENT_VERSION
+  echo LATEST_VERSION=$LATEST_VERSION
+  echo TAR_URL=$TAR_URL
+
+  download_src $TAR_URL
+  remove_image $EXISTING_IMAGE
+
+  echo "Собираем образ curl:$LATEST_VERSION..."
+  docker build . -t curl:$LATEST_VERSION
+
+  echo "Запускаем контейнер curl:$LATEST_VERSION..."
+  TEMP_CONTAINER_ID=$(docker create curl:$LATEST_VERSION)
+
+  echo "Копируем curl из контейнера"
+  rm -rf ~/bin/curl
+  docker cp $TEMP_CONTAINER_ID:/var/lib/app/curl/src/ ~/bin/curl
+
+  echo "Удаляем контейнер"
+  docker rm $TEMP_CONTAINER_ID
+
+  echo "Удаляем созданный образ"
+  docker rmi curl:$LATEST_VERSION
+
+  update_path $CURL_DIR
+}
+
+# Проверяем, было ли передано три аргумента
+if [ "$#" -ne 3 ]; then
+  echo "Ошибка: Необходимо передать 3 аргумента - текущую, новую версию curl и url src"
   exit 1
 fi
 
-CURL_DIR="$HOME/bin/curl"
-CURRENT_VERSION="$1"
-LATEST_VERSION="$2"
-EXISTING_IMAGE="curl:$CURRENT_VERSION"
-
-# Проверяем, содержит ли $PATH директорию $CURL_DIR
-if [[ ":$PATH:" == *":$CURL_DIR:"* ]]; then
-  echo "Директория $CURL_DIR уже содержится в PATH"
-else
-  # Если директория отсутствует, добавляем ее к $PATH
-  echo 'export PATH="$HOME/bin/curl:$PATH"' >> ~/.bashrc
-  echo "Директория $CURL_DIR добавлена к PATH"
-fi
-
-# Удаляем старый образ Docker, если он существует
-if [[ "$(docker images -q $EXISTING_IMAGE 2> /dev/null)" == "" ]]; then
-  echo "Старый образ $EXISTING_IMAGE не найден"
-else
-  echo "Удаляем старый образ $EXISTING_IMAGE..."
-  docker rmi $EXISTING_IMAGE
-fi
-
-echo "Собираем образ curl:$LATEST_VERSION..."
-docker build . -t curl:$LATEST_VERSION
-
-echo "Запускаем контейнер curl:$LATEST_VERSION..."
-TEMP_CONTAINER_ID=$(docker create curl:$LATEST_VERSION)
-
-echo "Копируем curl из контейнера"
-rm -rf ~/bin/curl
-docker cp $TEMP_CONTAINER_ID:/var/lib/app/curl/src/ ~/bin/curl
-
-echo "Удаляем контейнер"
-docker rm $TEMP_CONTAINER_ID
+main $1 $2 $3 
